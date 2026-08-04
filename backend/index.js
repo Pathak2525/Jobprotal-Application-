8038f4e1ddab7ace18a73bd674e30431572ac09e
Ishant Pathak <ishantpathak659@gmail.com>
2026-08-04 22:37:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-const corsOptions = {
-  origin: "http://localhost:5173",
+app.use(cors({
+  origin: "https://jobprotal-application-frontend.onrender.com",
   credentials: true,
-};
+}));
 
 // Middlewares
 app.use(cors(corsOptions));
